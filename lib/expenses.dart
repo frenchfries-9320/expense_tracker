1e0586ce10cbaf2446e60e91a7d29563f1a78d83
frenchfries-9320 <87230106+frenchfries-9320@users.noreply.github.com>
2024-07-08 23:30:54 +0900

    moved file

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,6 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/models/expenses_list.dart';
-import 'package:expense_tracker/models/widgets/new_expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
