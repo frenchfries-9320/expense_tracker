e893f9f89cf4236b3fca81653e81eb232ed18a11
frenchfries-9320 <87230106+frenchfries-9320@users.noreply.github.com>
2024-07-08 23:24:12 +0900

    add New data

--- lib/models/widgets/new_expense.dart
+++ lib/models/widgets/new_expense.dart
@@ -5,7 +5,9 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/widgets.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<StatefulWidget> createState() {
@@ -64,6 +66,14 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+
+    widget.onAddExpense(
+      Expense(
+          title: _titleController.text,
+          amount: enteredAmount,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
   }
 
   @override
